9f17bb7ec53b7efd30ff8bff04aab0f7186939e7
Yevhenii Marynets <imdanteasy@gmail.com>
2024-05-12 22:07:29 +0200

    Deploy

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import ReactDOM from "react-dom/client";
 import "@/app/global.css";
 import { Provider as StoreProvider } from "react-redux";
 import store, { persistor } from "@/app/store/store";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./app/app";
 import { PersistGate } from "redux-persist/integration/react";
 
@@ -11,10 +11,10 @@ const root = ReactDOM.createRoot(
 );
 root.render(
     <StoreProvider store={store}>
-        <BrowserRouter>
+        <HashRouter>
             <PersistGate loading={null} persistor={persistor}>
                 <App />
             </PersistGate>
-        </BrowserRouter>
+        </HashRouter>
     </StoreProvider>
 );
